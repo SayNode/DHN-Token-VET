2ec6b7a8184d666bc3f350104723d4551be340c0
RsTs23 <43611813+RsTs23@users.noreply.github.com>
2022-01-27 15:07:58 +0100

    Change error message

--- DHN.sol
+++ DHN.sol
@@ -10,7 +10,7 @@ contract DHN is ERC20() {
   * @param wallet Address of the wallet, where tokens will be transferred to
   */
   constructor(address wallet)  {
-    require(wallet != address(0),'No wallet was provided');
+    require(wallet != address(0),'Can't be zero wallet');
     _mint(wallet, 372000000 ether);
   }
 }
