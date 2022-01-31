999dd4caa30cf6b41c2e8fbdefe3c741c85c6f30
Werner Liechti <werner@saynode.ch>
2022-01-31 11:35:10 +0100

    Update DHN.sol

--- DHN.sol
+++ DHN.sol
@@ -10,7 +10,7 @@ contract DHN is ERC20() {
   * @param wallet Address of the wallet, where tokens will be transferred to
   */
   constructor(address wallet)  {
-    require(wallet != address(0),'Can't be zero wallet');
+    require(wallet != address(0),'Can not be zero wallet');
     _mint(wallet, 372000000 ether);
   }
 }
